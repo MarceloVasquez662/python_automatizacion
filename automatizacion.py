f33bc169ddaa7b73ed32cb46477334fb784c6427
MarceloVasquez662 <marcelovasquez662@gmail.com>
2021-12-17 17:17:00 -0300

    Version con creacion de carpetas

--- automatizacion.py
+++ automatizacion.py
@@ -4,10 +4,32 @@ import shutil
 usuario=os.getlogin()
 ruta_descargas="C:/Users/{}/Downloads/".format(usuario)
 
+#Creacion de carpetas si es necesario
+def crearCarpetas():
+    carpetas=[]
+
+    for dir in os.listdir(ruta_descargas):
+        nombre_carpeta, extension=os.path.splitext(dir)
+        if extension=="":
+            carpetas.append(nombre_carpeta) 
+
+    if "Video" or "Texto" or "Fotos" or "Otros" not in carpetas:
+        if "Video" not in carpetas:
+            os.makedirs(ruta_descargas + "Video")
+        if "Texto" not in carpetas:
+            os.makedirs(ruta_descargas + "Texto")
+        if "Fotos" not in carpetas:
+            os.makedirs(ruta_descargas + "Fotos")
+        if "Otros" not in carpetas:
+            os.makedirs(ruta_descargas + "Otros")
+
+
+#Definicion de extensiones de archivos
 ext_foto=[".png",".jpg",".jpeg",".gif",".psd",".bmp",".svg"]
 ext_video=[".mp4",".avi",".mkv",".flv",".mov",".wmv",".divx"]
 ext_texto=[".docx",".doc",".txt",".pdf",".pptx"]
 
+#Funcion ordenar archivos segun extension
 def ordenarArchivos(archivo, extension):
     move=False
     for i in ext_texto:
@@ -26,7 +48,9 @@ def ordenarArchivos(archivo, extension):
     if extension!="" and move==False:
         shutil.move(ruta_descargas+archivo, ruta_descargas+"Otros")
 
+#Funcion definir nombre y extension y llamar a Ordenar
 def main():
+    crearCarpetas()
     for archivo in os.listdir(ruta_descargas):
         nombre_archivo, extension = os.path.splitext(archivo)
         ordenarArchivos(archivo, extension)
